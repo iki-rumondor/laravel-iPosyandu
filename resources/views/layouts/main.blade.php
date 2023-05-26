18d6786d3c2ece68f0df7d16f1f6146a47fb500d
iRumondor <ikirumondor07@gmail.com>
2023-05-26 21:09:52 +0800

    fix sidebar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,16 +16,9 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
 
-    <!-- Sweet Alert -->
     <link type="text/css" href="{{ asset('vendor/sweetalert2/dist/sweetalert2.min.css') }}" rel="stylesheet">
-
-    <!-- Notyf -->
     <link type="text/css" href="{{ asset('vendor/notyf/notyf.min.css') }}" rel="stylesheet">
-
-    <!-- Volt CSS -->
     <link type="text/css" href="{{ asset('css/volt.css') }}" rel="stylesheet">
-
-    <!-- NOTICE: You can use the _analytics.html partial to include production code specific code & trackers -->
 
 </head>
 
--- resources/views/posyandu/schedule/index.blade.php
+++ resources/views/posyandu/schedule/index.blade.php
@@ -149,6 +149,29 @@
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach ($schedules as $schedule)
+                            <tr>
+                                <td>{{ $schedule->posyandu->name }}</td>
+                                <td>{{ $schedule->date }}</td>
+                                <td><span
+                                        class="badge {{ $schedule->status == 'Terjadwal' ? 'bg-primary' : 'bg-success' }}">{{ $schedule->status }}</span>
+                                </td>
+                                <td>
+
+                                    <button class="btn-delete btn btn-sm btn-danger animate-up-3" data-bs-toggle="modal"
+                                        data-bs-target="#deleteModal" data-id="{{ $schedule->id }}">
+                                        <svg data-bs-toggle="tooltip" data-bs-placement="top" title="Hapus Data Posyandu"
+                                            class="icon icon-xs" fill="none" stroke="currentColor" stroke-width="1.5"
+                                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0">
+                                            </path>
+                                        </svg>
+                                    </button>
+
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
@@ -180,8 +203,9 @@
                                             clip-rule="evenodd"></path>
                                     </svg>
                                 </span>
-                                <input name="date" id="date" data-datepicker="" class="form-control datepicker-input"
-                                    id="schedule" type="text" placeholder="Bulan/Tanggal/Tahun" required>
+                                <input name="date" id="date" data-datepicker=""
+                                    class="form-control datepicker-input" id="schedule" type="text"
+                                    placeholder="Bulan/Tanggal/Tahun" required>
                                 <div class="datepicker datepicker-dropdown datepicker-orient-left datepicker-orient-bottom"
                                     style="left: 1398.97px; top: 304.172px;">
                                     <div class="datepicker-picker">
@@ -344,6 +368,11 @@
                 })
             $('button[type=submit]').removeAttr('disabled')
 
+        })
+
+        $('.btn-delete').click(function() {
+            const id = $(this).data('id')
+            $('#deleteModal form').attr('action', '/posyandu/schedule/' + id)
         })
     </script>
 @endpush
